b7566efac360cbdaabc828ae3760f86ca9fff18f
Cuncun24 <jhoelgamer.24@gmail.com>
2026-02-10 15:19:18 -0500

    configurando segunda vez xq no sube a produccion

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    host: true,
-    port: Number(process.env.PORT),
-    allowedHosts: "all"
-  }
+  plugins: [react()]
 });
