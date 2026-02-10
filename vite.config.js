ec008235475463d5296382733753712ae7fcc836
Cuncun24 <jhoelgamer.24@gmail.com>
2026-02-10 15:06:42 -0500

    cambiando vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  preview: {
+    host: true,
+    port: process.env.PORT,
+    allowedHosts: "all"
+  }
+});
